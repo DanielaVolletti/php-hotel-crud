438dffb40009d1d7045da63f80871dc31e8c71e2
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-05-22 18:06:30 +0200

    aggiunta handlebars

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ function getPagamenti() {
     url: "getPagamenti.php",
     method: "GET",
     success: function (data) {
-      console.log(data);
+      // inizializzo variabili handlebars
+      var source = $("#pagamenti-template").html();
+      var template = Handlebars.compile(source);
+      // ciclo for of per estrapolare prezzo e status pagamento
       for (var pagamenti of data) {
-        var status = pagamenti.status;
+        var status = pagamenti.status + " , ";
         var prezzo = pagamenti.price;
-        $('#info-pagamenti').append("<li>" + status + " , " + prezzo + "</li>");
+        var context = {
+          pagamento: status + prezzo
+        };
+        var html = template(context);
+        $('#info-pagamenti').append(html);
       }
     }, error: function (error) {
         console.log(error);
